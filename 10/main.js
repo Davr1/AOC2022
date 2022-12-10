8d3179922db4db3e6042b204f4208a02c4346c0d
Davr1 <42148912+Davr1@users.noreply.github.com>
2022-12-10 09:55:02 +0100

    Day 10

---
+++ 10/main.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const { join } = require("path");
+
+const data = fs.readFileSync("./input", (err, data) => data).toString();
+
+let cpuLog = {x: []};
+let registers = {x: 1};
+
+data.split("\n").forEach(instruction => {
+    let [inst, value] = instruction.split(" ");
+    switch(inst) {
+        case "addx":
+            cpuLog.x.push(registers.x);
+            cpuLog.x.push(registers.x);
+            registers.x += +value;
+            break;
+        case "noop":
+        default:
+            cpuLog.x.push(registers.x);
+    }
+})
+
+// Part 1
+let signalStrength = 0;
+[20, 60, 100, 140, 180, 220].forEach(c => signalStrength += cpuLog.x[c-1] * c)
+
+console.log(signalStrength);
+
+// Part 2
+let screen = ["", "", "", "", "", ""];
+
+for (let i = 0; i < 40 * 6; i++) {
+    let pixel = ".";
+    if (cpuLog.x[i] + 1 >= i % 40 && cpuLog.x[i] - 1 <= i % 40) pixel = "#";
+    
+    screen[Math.floor(i / 40)] += pixel;
+}
+
+console.log(screen.join("\n"));
